9690676045f7e1e942c039a619218a6b2dc5abed
HowyoungZhou <howyoung3@gmail.com>
2019-04-17 23:56:54 +0800

    添加图形库

---
+++ test.c
@@ -0,0 +1,22 @@
+#include "graphics.h"
+#include "extgraph.h"
+#include "genlib.h"
+#include "simpio.h"
+#include "random.h"
+#include "strlib.h"
+#include "conio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <windows.h>
+#include <olectl.h>
+#include <mmsystem.h>
+#include <wingdi.h>
+#include <ole2.h>
+#include <ocidl.h>
+#include <winuser.h>
+#include <stdbool.h>
+
+void Main() {
+    InitGraphics();
+}
