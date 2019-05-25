1e09e8b63bd2670d009c620da0b9ed64e9076ea7
HowyoungZhou <howyoung3@gmail.com>
2019-05-25 23:14:37 +0800

    使用位图资源渲染 Pellet

--- sprites/pellet_sprite.c
+++ sprites/pellet_sprite.c
@@ -2,23 +2,36 @@
 #include <sprite.h>
 #include <extgraph.h>
 #include <drawing.h>
+#include <assets.h>
 #include "pellet_sprite.h"
 
+static unsigned int _spriteCount = 0;
+static BitmapAsset *_asset = NULL;
+
 static void _Render(Sprite *this) {
-    SetPenColor("Pellet");
-    StartFilledRegion(1.);
-    DrawVectorCircle(CalcCentre(this), CalcIncircleRadius(this));
-    EndFilledRegion();
+    DrawBitmapAsset(_asset, this->position, this->size);
 }
 
 static void _Collide(Sprite *this, int id, Sprite *other) {
     this->visible = false;
 }
 
+static void _Destruct(Sprite *this) {
+    _spriteCount--;
+    if (_spriteCount == 0 && _asset != NULL) {
+        FreeBitmapAsset(_asset);
+        _asset = NULL;
+    }
+    DestructSprite(this);
+}
+
 Sprite *ConstructPellet(Vector2 position, Vector2 size) {
+    _spriteCount++;
     Sprite *obj = ConstructSprite(position, size, ZERO_VECTOR);
-    obj->renderer.Render = _Render;
+    if (_asset == NULL) _asset = LoadBitmapAsset("pellet.bmp");
     RegisterCircleCollider(obj, DEFAULT_COLLIDER_ID, false, CalcRelativeCentre(obj), CalcIncircleRadius(obj));
+    obj->renderer.Render = _Render;
     obj->Collide = _Collide;
+    obj->Destruct = _Destruct;
     return obj;
 }
