efcd4eaa5289425ce5a8c47305e140396796a1d7
MeU1024 <1542694891@qq.com>
2019-06-07 17:25:24 +0800

    增加成功与失败对话框

--- ui/textbox.c
+++ ui/textbox.c
@@ -8,19 +8,39 @@
 
 static int   _GAMESTART = 0;
 static char memo[80]="NAME";
-static double cx,cy;
+static double cx,cy,fH;
+
+void DrawWinTextBox(){
+    cx = GetWindowWidth();
+    cy = GetWindowHeight();
+    double fH = GetFontHeight();
+
+    //绘制成功对话框
+    drawRectangle(cx/2-2, cy/2-1,4, 2.5, 1);
+    SetPenColor("Brown");
+    drawLabel(cx/2-TextStringWidth("Congratulations!")/2, cy/2+ 1 , "Congratulations!");
+    drawLabel(cx/2-TextStringWidth("Please enter your name!")/2, cy/2+0.8, "Please enter your name!");
+    textbox(GenUIID(0), cx/2-1.5, cy/2, 3, fH*2.5, memo, sizeof(memo));
+}
+
+void DrawLoseTextBox(){
+
+    cx = GetWindowWidth();
+    cy = GetWindowHeight();
+    double fH = GetFontHeight();
+
+    //绘制失败对话框
+    drawRectangle(cx/2-2, cy/2-1,4, 2.5, 1);
+    SetPenColor("Brown");
+    drawLabel(cx/2-TextStringWidth("Your score is")/2, cy/2 + 1 , "Your score is");
+    drawLabel(cx/2-TextStringWidth("Good luck next time!")/2, cy/2 , "Good luck next time!");
+
+}
 
 void _Render()
 {
-    double fH = GetFontHeight();
-    cx = GetWindowWidth();
-    cy = GetWindowHeight();
 
-    //绘制对话框
-    drawRectangle(cx/2-2, cy/2-1,4, 2.5, 1);
-    SetPenColor("Brown");
-    drawLabel(cx/2-TextStringWidth("ENTER YOUR NAME!")/2, cy/2+1, "ENTER YOUR NAME!");
-    textbox(GenUIID(0), cx/2-1.5, cy/2, 3, fH*2.5, memo, sizeof(memo));
+    DrawLoseTextBox();
 
     if (button(GenUIID(0), cx/2+1, cy/2-0.8, 0.5, 0.4,"OK"))
         _GAMESTART = !_GAMESTART;
